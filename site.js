cf6bd18f3c22031eba48eeb4c901c316b4ba6a1b
Neil Ebrey <neil.ebrey@gmail.com>
2016-02-08 23:28:27 +0100

    add salvattore

--- site.js
+++ site.js
@@ -1,8 +1,9 @@
 var chans = [
-  'freecodecamp', 'cheatbanned', 'terakilobyte', 'habathcx',
-  'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff',
-  'freecodecamp', 'cheatbanned', 'terakilobyte', 'habathcx',
-  'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff'
+  'freecodecamp', '2dheroes',
+  'noobs2ninjas', 'simuleios',
+  'trumpi27', 'cmiotk1863', 'kpulv', 'adam13531',
+  'Bamboy360', 'noobs2ninjas', 'kashou_', 'destinybit',
+  'flipswitchx', 'zhootdotse', 'gamingcatsstudio'
 ];
 var results = [];
 var streamURL = 'https://api.twitch.tv/kraken/streams/{{CHAN}}?callback=?';
@@ -10,34 +11,24 @@ var streamURL = 'https://api.twitch.tv/kraken/streams/{{CHAN}}?callback=?';
 var htmlString = '';
 var $grid
 
-var panels = ['grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3'];
+// var panels = ['grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3', 'grid-item', 'grid-item--width2', 'grid-item--width3'];
+var panels = ['item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item'];
 
 $(document).ready(function() {
-  $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
-  });
-  $grid.imagesLoaded().progress(function() {
-    $grid.masonry();
-  });
   getData();
-
 });
 
 function getData() {
+  chans = shuffle(chans);
   // while (results.length !== chans.length) {
   for (var i = 0; i < chans.length; i++) {
     var url = streamURL.replace('{{CHAN}}', chans[i]);
 
     $.getJSON(url, function(data) {
       getChanInfo(data._links.channel, function(result) {
+        console.log(result)
         data.chanInfo = result;
-        console.log(data)
         results.push(buildPanel(data));
-        // $grid.imagesLoaded().progress(function() {
-        //   $grid.masonry('layout');
-        // });
         checkFinished();
       });
     });
@@ -46,12 +37,29 @@ function getData() {
 
 function checkFinished() {
   if (results.length === chans.length) {
+
+    var masonry_container = document.querySelector('#timeline');
+    var new_items = []; // array to contain our DOM elements
+    // for each element we want to add, selected using jQuery, append it to our array
+
+    // use [0] to get at the DOM element
+
+
+
+
     htmlString = '';
     for (var i = 0; i < results.length; i++) {
-      htmlString += results[i];
+      var item = $.parseHTML('<div></div>');
+      console.log(results[i])
+      console.log('---------')
+      $(item).html(results[i]);
+      //new_items.push($('<div/>').html(results[i]).contents()); //  results[i];
+      salvattore.appendElements(masonry_container, item);
     }
 
-    $('#content').html(htmlString)
+
+
+    //$('#timeline').html(htmlString)
 
 
     console.log(htmlString)
@@ -64,30 +72,44 @@ function getChanInfo(url, callback) {
   });
 }
 
+
+//Fisher-Yates
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function buildPanel(x) {
   var html = '';
-  //html += '<div class="row">'
   console.log(Math.floor((Math.random() * 10)));
   html += '<div class="' + panels[Math.floor((Math.random() * 10))] + '">'
-    //html += '<div class="col-sm-4 col-xs-4 text-right bordercol">'
-    //html += '<div class="grid-item">'
-  html += '<img class="img-responsive" src="' + (x.chanInfo.logo || 'images/404_preview-300x300.png') + '"/>'
   html += '<a target="_blank" href="' + 'http://twitch.tv/' + x.chanInfo.display_name + '">'
-  html += '<h4 class="title">'
+  html += '<h4 class="title text-center">'
   html += x.chanInfo.display_name
   html += '</h4>'
   html += '</a>'
-    //html += '</div>' //first col
-    //  html += '<div class="col-md-8 col-sm-8 col-xs-8 borderleft">'
-
-  //html += '<div class="storyDesc">' + story.metaDescription + '</div>'
-  //html += '<div class="storyPosted"><i class="fa fa-calendar-o fa-fw"></i>' + new Date(story.timePosted) + '</div>'
-  //html += '<div class="upVotes"><i class="fa fa-thumbs-o-up fa-fw"></i>' + story.upVotes.length + '</div>'
-  //html += '<a target="_blank" href="http://freecodecamp.com/' + story.author.username + '">'
-  //html += '<span><i class="fa fa-fire fa-fw"></i>' + story.author.username + '</span>'
-  //html += '</a>'
+  html += '<img src="' + (x.chanInfo.profile_banner || x.chanInfo.logo || 'images/404_preview-300x300.png') + '"/>'
+  html += '<a target="_blank" href="' + 'http://twitch.tv/' + x.chanInfo.display_name + '">'
+  html += '<h4 class="title text-center">'
+  html += x.chanInfo.display_name
+  html += '</h4>'
+  html += '</a>'
   html += '</div>' //second col
-    //html += '</div>' //row
   return html
 
 }
